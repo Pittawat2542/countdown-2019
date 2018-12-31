259ac2f3ae36e6f56d810623d4352fd9f0e62688
Pittawat2542 <pittawat2542@hotmail.com>
2018-12-31 21:56:41 +0700

    [ADD] Complete countdown timer

---
+++ script.js
@@ -0,0 +1,28 @@
+let timer = document.querySelector("#timer");
+let hours = document.querySelector("#hours");
+let minutes = document.querySelector("#minutes");
+let seconds = document.querySelector("#seconds");
+
+let time = setInterval(() => {
+  let duration = moment.duration(
+    moment("2019-01-01 00:00:00").valueOf() - moment().valueOf()
+  );
+
+  if (duration >= 0) {
+    hours.textContent =
+      duration.hours().toString().length === 1
+        ? "0" + duration.hours().toString()
+        : duration.hours().toString();
+    minutes.textContent =
+      duration.minutes().toString().length === 1
+        ? "0" + duration.minutes().toString()
+        : duration.minutes().toString();
+    seconds.textContent =
+      duration.seconds().toString().length === 1
+        ? "0" + duration.seconds().toString()
+        : duration.seconds().toString();
+  } else {
+    timer.textContent = "Happy New Year 2019";
+    clearInterval(time);
+  }
+}, 1000);
